875a89f03642fbc03eb4997f2d25d3d33305d4a8
mdmunna84880 <mdmunna19434@gmail.com>
2025-10-07 13:58:12 +0530

    Day 1: Setup and backend connection complete

--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -1,9 +1,22 @@
 import { Router } from "express";
-import createTask from '../controllers/taskController.js';
+import {createTask, getTasks, getTaskById, updateTask, deleteTask} from '../controllers/taskController.js';
 
 
 const router = Router();
 
+// Create a tasks
 router.post("/api/tasks", createTask);
+
+// Show the tasks
+router.get("/api/tasks", getTasks);
+
+// Show the specific task
+router.get("/api/tasks/:id", getTaskById);
+
+// Update the task
+router.put("/api/tasks/:id", updateTask);
+
+// Delete the task
+router.delete("/api/tasks/:id", deleteTask);
 
 export default router;
